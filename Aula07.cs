fb27a85683f84cb378c352e31389284ebd302fae
Matheus Jesus <thequeira@gmail.com>
2022-06-16 10:27:09 -0300

    aula 07 commit

---
+++ Aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula07{
+
+    static void Main(){
+
+        const string semana = "sabado, domingo, segunda,terça, quarta, quinta";
+
+        Console.WriteLine("Dia da semana {0}",semana);
+    }
+
+}
