240397f29b25be1b1f9c34abce08fcb297ea15d7
Matheus Jesus <thequeira@gmail.com>
2022-06-16 23:41:59 -0300

    commit09

---
+++ Aula09.cs
@@ -0,0 +1,19 @@
+using System;
+
+//Usando operador Bitwise 
+class aula09{
+
+    static void Main(){
+
+        int n1;
+
+        n1 = 10;
+        n1= n1>>1;
+        Console.WriteLine("O operado valeria 10, mas com bitWise ele vai valer metade {0}",n1);
+
+
+
+    }
+
+
+}
