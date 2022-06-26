d7daabb4abe3a5dcecb7c67560c7cb6305d33ff2
Matheus Jesus <thequeira@gmail.com>
2022-06-26 11:39:11 -0300

    commite2

---
+++ aula19.cs
@@ -0,0 +1,22 @@
+//comandos de interações//
+// Sempre que precisar repetir um bloco de comando, uma atividade ou serie de comando use os comandos de repeticao//
+
+
+using System;
+class aula1{
+    static void Main(){
+
+//O looping FOR é dividido em três partes//
+//Expressão 1, 2 e 3. 
+//Expressao 1 indica o incio da repetição num=0
+//Expressao 2 indica a quantidade de repetições, Eu Matheus Js gosto de dizer que a expresão dois é o limitador, limitarei quando chegar em 10.
+//Expressão 3, é o sequenciador, qual será a sequência ? num++ ou num = num+1 ou então num = num+2 ou até mesmo num +=1
+        for(int num=0;num<10;num++){
+        Console.WriteLine("Está safo ? {0}",num);
+        
+        }
+
+
+
+}
+}
