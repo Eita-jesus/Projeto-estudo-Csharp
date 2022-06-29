2f747018927d8e7f54ac483b03f4a82688f6b49f
Matheus Jesus <thequeira@gmail.com>
2022-06-28 23:14:24 -0300

    Imagem fluxograma While

---
+++ aula21.cs
@@ -0,0 +1,36 @@
+//comandos de interações//
+// Sempre que precisar repetir um bloco de comando, uma atividade ou serie de comando use os comandos de repeticao//
+
+
+using System;
+class aula1{
+    static void Main(){
+
+/* Utilizando o comando WHILE , tranduzindo ... ENQUANTO
+Simplificando, ENQUANTO(i for menor que 10) faça algo nesse caso escreva 
+
+ */
+        
+        int [] num=new int[10];
+        String senha, senha1;
+
+        do{
+            Console.WriteLine("Digite uma senha:");
+            senha = Console.ReadLine();
+            senha1 ="123456";
+
+
+        }while(senha != senha1);{
+            Console.WriteLine("Senha autenticada com sucesso!");
+
+
+        }
+
+
+
+        }
+
+    }
+
+
+
