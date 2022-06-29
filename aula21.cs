fd1e7a946e33e2cb4038e9a1af6a4ad642b0c981
Matheus Jesus <thequeira@gmail.com>
2022-06-28 23:21:22 -0300

    Ajuste na observação da aula 21 comentário ajustado

--- aula21.cs
+++ aula21.cs
@@ -9,8 +9,13 @@ class aula1{
 /* Utilizando o comando WHILE , tranduzindo ... ENQUANTO
 Simplificando, ENQUANTO(i for menor que 10) faça algo nesse caso escreva 
 
- */
+*/
         
+
+// Observação muito importante no While: Além disso, lembre-se de inserir no bloco de código a lógica necessária para que a condição se torne falsa em alguma execução do loop. Por isso, usei no comando abaixo o operador diferente != e não igual == 
+//Caso tenha mais dúvida olhe o while.png no Git.
+
+
         int [] num=new int[10];
         String senha, senha1;
 
